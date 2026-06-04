c1ea1902428cd34e59b5749790e433b3ec7b57c1
Cavs <miguelferreiranoss@gmail.com>
2026-06-04 20:36:19 +0100

    Refactor service worker cache handling
    Updated service worker to use dynamic cache name and simplified cache deletion logic.

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
-const CACHE = 'digi-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'digi-v' + Date.now();
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
